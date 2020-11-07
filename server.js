83839424676a3e72e5e4e45daf032761da5234ff
vormc2005 <voronov.dmitry@gmail.com>
2020-11-06 21:06:03 -0500

    getting ready to connect email

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const bodyparser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors =require('cors')
 //Import routes
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
@@ -14,6 +16,7 @@ const connectDB = require('./config/db');
 connectDB();
 
 //Middlewares
+app.use(bodyparser.json());
 app.use(express.json({extended: false}));
 
 
@@ -25,7 +28,56 @@ app.use('/api', authRoutes);
 app.use('/api',inquiryRoutes);
 app.use('/api', backUpRoutes);
 
-const PORT = process.env.PORT || 3000;
+//*************************Email******************** */
+// app.post('/api/forma', (req, res)=>{
+//     let data = req.body
+//     console.log(data)
+//     let smtpTransport = nodemailer.createTransport({
+//         service: "Gmail",
+//         port:465,
+//         auth: {
+//             user:"carcheckers2020@gmail.com",
+//             pass: "Carcheckers#123"
+//         }
+//     });
+
+//     let mailOptions ={
+//         from: data.email,
+//         to: "carcheckers2020@gmail.com",
+//         subject: `Message from  ${data.name}`,
+//         html: `
+//         <h3>Information</h3>
+//         <ul>
+//             <li>Name: ${data.name}</li>
+//             <li>Subject: ${data.subject}</li>
+//             <li>Email: ${data.email}</li>
+//             <li>Phone: ${data.phone}</li>
+//             <li>Year: ${data.year}</li>
+//             <li>Make: ${data.make}</li>
+//             <li>Model: ${data.model}</li>
+//             <li>Trim: ${data.trim}</li>
+           
+//         </ul>
+
+//         <h3>Comments</h3>
+//         <p>${data.comments}</p>
+//         `
+//     }
+
+//     smtpTransport.sendMail(mailOptions, (error, response)=>{
+//         if(error){
+//             res.send(error)
+//         }
+//         else{
+//             res.send('Success')
+//         }
+//     })
+
+//     smtpTransport.close();
+
+// })
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, ()=>console.log(`Seerver started on port ${PORT}`))
 
